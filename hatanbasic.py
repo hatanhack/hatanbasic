356f9e7fcfb0b3bc889667bd32a244f37372503e
HATANHack <ebrahimalsmiri12345@gmail.com>
2025-09-08 01:15:32 +0300

    Update hatanbasic.py

--- hatanbasic.py
+++ hatanbasic.py
@@ -25,33 +25,30 @@ def main():
 | This Tool Installs Basic Packages    |
 +--------------------------------------+
 |   Coded By : hatanhack               |
-|   Version  : 2.0                     |
+|   Version  : 3.0 (Termux Fixed)      |
 +--------------------------------------+''')
 
     slowprint(''' \033[93m
 [01] python
 [02] python2
-[03] python-dev
-[04] python3
-[05] php
-[06] java
-[07] git
-[08] perl
-[09] bash
-[10] nano
-[11] curl
-[12] openssl
-[13] openssh
-[14] wget
-[15] clang
-[16] nmap
-[17] w3m
-[18] hydra
-[19] ruby
-[20] macchanger
-[21] host
-[22] dnsutils
-[23] coreutils
+[03] php
+[04] openjdk-17 (java)
+[05] git
+[06] perl
+[07] bash
+[08] nano
+[09] curl
+[10] openssl
+[11] openssh
+[12] wget
+[13] clang
+[14] nmap
+[15] w3m
+[16] hydra
+[17] ruby
+[18] dnsutils (host command)
+[19] net-tools (macchanger alternative)
+[20] coreutils
 ''')
 
     choice = input("\033[93mDo you want to install all packages [y/n] : ").strip().lower()
@@ -62,12 +59,16 @@ def main():
         # Update packages
         os.system("pkg update -y && pkg upgrade -y")
 
-        # Install packages
+        # Install packages (fixed names)
         packages = [
-            "python", "python2", "python-dev", "python3", "php", "java", "git",
-            "perl", "bash", "nano", "curl", "openssl", "openssh", "wget", "clang",
-            "nmap", "w3m", "hydra", "ruby", "macchanger", "host", "dnsutils", "coreutils"
+            "python", "python2", "php", "openjdk-17", "git", "perl", "bash", "nano",
+            "curl", "openssl", "openssh", "wget", "clang", "nmap", "w3m", "hydra",
+            "ruby", "dnsutils", "net-tools", "coreutils"
         ]
+
+        # Enable repo for hydra if not installed
+        os.system("pkg install -y x11-repo")
+
         os.system("pkg install " + " ".join(packages) + " -y")
 
         print("\n\033[92mAll packages installed successfully!\n")

3929cb277b0b396f5f8ad9eb695d5b53185f625e
HATANHack <ebrahimalsmiri12345@gmail.com>
2025-09-15 15:12:30 +0300

    Update install-ubuntu-hatan.sh

--- install-ubuntu-hatan.sh
+++ install-ubuntu-hatan.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+# Script to install or update the Ubuntu environment and run HATAN BASIC
 
+# Download or update the Neo-Oli repository
+if [ -d "$HOME/termux-ubuntu" ]; then
+    cd "$HOME/termux-ubuntu"
+    git pull origin master
+else
+    git clone https://github.com/Neo-Oli/termux-ubuntu.git
+fi
+
+# Install or update Ubuntu
+cd "$HOME/termux-ubuntu"
+bash ubuntu.sh
+
+# Launch Ubuntu and run HATAN BASIC
+./start-ubuntu.sh <<EOF
+cd /root/hatanbasic
+python3 hatanbasic.py
+EOF

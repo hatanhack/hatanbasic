2879fef8b859efd9a087af3e3b751279ff4d0eca
HATANHack <ebrahimalsmiri12345@gmail.com>
2025-09-15 15:51:16 +0300

    Update install-ubuntu-hatan.sh

--- install-ubuntu-hatan.sh
+++ install-ubuntu-hatan.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-# Script to install or update the Ubuntu environment and run HATAN BASIC
+# Script to install or update Ubuntu in Termux
 
-# Download or update the Neo-Oli repository
-if [ -d "$HOME/termux-ubuntu" ]; then
-    cd "$HOME/termux-ubuntu"
+# Update Termux packages
+pkg update -y && pkg upgrade -y
+pkg install wget proot tar git python -y
+
+# Clone or update Neo-Oli Ubuntu repository inside your project folder
+if [ -d "$HOME/hatanbasic/termux-ubuntu" ]; then
+    cd "$HOME/hatanbasic/termux-ubuntu"
     git pull origin master
 else
-    git clone https://github.com/Neo-Oli/termux-ubuntu.git
+    git clone https://github.com/Neo-Oli/termux-ubuntu.git "$HOME/hatanbasic/termux-ubuntu"
+    cd "$HOME/hatanbasic/termux-ubuntu"
 fi
 
-# Install or update Ubuntu
-cd "$HOME/termux-ubuntu"
+# Run ubuntu.sh to install and start Ubuntu
 bash ubuntu.sh
 
-# Launch Ubuntu and run HATAN BASIC
-./start-ubuntu.sh <<EOF
-cd /root/hatanbasic
-python3 hatanbasic.py
-EOF
+echo "Ubuntu installation complete! You are now inside Ubuntu environment."
